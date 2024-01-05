5a29309e950ac528516e73ff7f93072b1229db0f
bax2004rj <bax2004rj@gmail.com>
2024-01-05 18:00:48 -0500

    Improvements to /proc/ reader

--- Drivers/Linux/src/cpulightSERVER.c
+++ Drivers/Linux/src/cpulightSERVER.c
@@ -9,14 +9,20 @@ char serialLocation[32] = "/dev/ttyACM0"; //Serial device
 int temps[2] = {70,100}; //Temperatures to turn temp warning lights off
 int fanThreshold = 1000; //Deviation in RPM for when to sound fan alarm
 int safeShutdown = 1; // Send safe safe shutdown signal when process killed
-int delayTime = 1; // Delay in ms for the lights
+int delayTime = 1; // Delay in s for the lights
 char versionString[32] = "V1.0.0B";
+int stopRunning = 0;
 
 int interpretCommandLineArgs(int argc, char argv[]);
 
 int main(int argc, char argv[]){
     FILE *serialpoint = fopen(serialLocation,"rw");
-    printf("\033[32m[ PASS  ]\033[37m: Serial connected!\n"); //this will be mor e complex in future
+    if(serialpoint == -1){
+        stopRunning = 1;
+        printf("\033[31m[ FATAL ]\033[37m: Serial port not connected.\n");
+    } else {
+        printf("\033[32m[ PASS  ]\033[37m: Serial connected!\n"); //this will be mor e complex in future
+    }
     int argMode = interpretCommandLineArgs(argc, argv);
     if(argMode == 1){
         confUi(serialLocation,temps, fanThreshold,safeShutdown);
@@ -24,9 +30,10 @@ int main(int argc, char argv[]){
         char thermThrowaway;//Here because C doesnt have default options
         readConf(serialLocation,temps,fanThreshold,safeShutdown,thermThrowaway);
         printf("\033[32m[ PASS  ]\033[37m: Config file read\n");
-        fputc('b',serialpoint);
+        fputs("br",serialpoint);
         printf("\033[32m[ PASS  ]\033[37m: daemon started!\n");
         while (1){
+            puts("updating light");
             sleep(delayTime);
             //Update CPU light
             int cpu = readCpuInfo(255);
@@ -42,7 +49,7 @@ int main(int argc, char argv[]){
 
 int interpretCommandLineArgs(int argc, char argv[]){//Read inputs from command line
     int option;
-    while((option = getopt(argc,argv,"sv"))!=-1){
+    while((option = getopt(argc,argv,"csv"))!=-1){
         switch (option)
         {
         case 'c':
--- Drivers/Linux/src/systemReader.h
+++ Drivers/Linux/src/systemReader.h
@@ -3,27 +3,54 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int readCpuInfo(int scale){ //Reads data from /proc/stat. Scale indicates how much to multiply by when all data is found (ex. 100 for percentages, 255 for PWM values)
-    //Update CPU light
+int previousTimeSum = 0;
+int previousIdle = 0;
+int timeSum = 0;
+int idleTime = 0;
+
+int readCpuInfo(int scale){ //Reads data from /proc/stat. Scale indicates how much to multiply by when all data is found (ex. 100 for percentages, 255 for PWM values).
+    puts("reading cpu");
+    previousTimeSum = timeSum; //Original attempt at code
+    previousIdle = idleTime;
+    timeSum = 0;
+    idleTime = 0;
+    // Open /proc/stat
     FILE *cpuUsage = fopen("/proc/stat","r");
     // Read /proc/stat
     // skip first item "cpu", sum first line
-    char throwawayData[5];
-    fgets(throwawayData,5,cpuUsage);
+    char throwawayData[6];
+    fgets(throwawayData,6,cpuUsage);
     // get all numbers in first line
     int values[10];
-    fscanf(cpuUsage,"%d %d %d %d %d %d %d %d %d %d",&values[0],&values[1],&values[2],&values[3],&values[4],&values[5],&values[6],&values[7],&values[8],&values[9]);// Longest scanf ever!
+    for(int i = 0;i<10;i++){// fscanf didnt exactly work so were taking it on our own hands
+        char data[32]="\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
+        char tmp = '0';
+        puts("reading data");
+        for(int j = 0;isalnum(tmp);j++){
+            data[j] = fgetc(cpuUsage);
+            tmp = data[j];
+            printf("char %d = %c",j,data[j]);
+        }
+        values[i] = atoi(data);
+        printf("data at %d = %d ",i,values[i]);
+        //fgetc(cpuUsage); //Skip the space
+    }
+    printf("\n");
+    //fscanf(cpuUsage,"%d %d %d %d %d %d %d %d %d %d",&values[0],&values[1],&values[2],&values[3],&values[4],&values[5],&values[6],&values[7],&values[8],&values[9]);// Longest scanf ever!
     // add all numbers
-    int timeSum = 0;
+    timeSum = 0;
     for(int i = 0; i<10; i++){
         timeSum += values[i];
     }
+    idleTime = values[4];
     // Divide idle (4th item) by total
-    float idlePercent = values[3]/timeSum;
+    float idlePercent = (idleTime-previousIdle)/(timeSum-previousTimeSum);
     // Subtract from 1 
     float nonIdlePercent = 1-idlePercent;
     //Multiply by scale
     nonIdlePercent *= scale;
-    printf("total: %d values: %d\n",timeSum,values[3]);
+    printf("total: %d idle time: %d\n",timeSum,values[3]);
+    //Close before reporting
+    fclose(cpuUsage);
     return (int) nonIdlePercent;
 }
